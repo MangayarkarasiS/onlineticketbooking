34659f18ef96d328e03942a5ac82218247e682fa
Trainer Mangai <mangai2000@gmail.com>
2024-03-17 08:13:08 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+ 
+
+
+
+
+
+/*{path:'', redirectTo:'bus-list',pathMatch:'full'},
+  {path:'bus-list',component:BusListComponent},
+  {path:'bus-route-list/:bid',component:BusRouteListComponent}*/
